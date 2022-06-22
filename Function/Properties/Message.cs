54212ae3919db94082e967959e2c6b543b339346
joerivanarkel <Joeri@vanarkel.net>
2022-06-22 13:01:08 +0200

    hihi haha

---
+++ Function/Properties/Message.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ProcessAndDownloadFilePOC
+{
+    public class Message
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        
+    }
+}
--- Function/QueueTrigger.cs
+++ Function/QueueTrigger.cs
@@ -2,17 +2,22 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace ProcessAndDownloadFilePOC
 {
     public class QueueTrigger
     {
         [FunctionName("QueueTrigger")]
-        public void Run(
+        [return: ServiceBus("myqueue", Connection = "ServiceBusConnection")]
+        public string Run(
             [QueueTrigger("message", Connection = "processanddownloadfile_STORAGE")]string myQueueItem, ILogger log
         )
         {
+            var deserializedmessage = JsonConvert.DeserializeObject<Message>(myQueueItem);
             log.LogInformation($"C# Queue trigger function processed");
+            log.LogInformation($"Guid: {deserializedmessage.Id}");
+            return myQueueItem;
         }
     }
 }
